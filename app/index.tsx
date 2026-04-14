546f3cb8a1d735b8ba38c48b0d8267f0456d8404
Rup-23 <152003973+Rup-23@users.noreply.github.com>
2026-04-15 00:01:50 +0530

    Initial

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hey Rupesh this is a simple React Native app!</Text>
+    </View>
+  );
+}
